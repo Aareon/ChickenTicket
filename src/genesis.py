5006f263e4cc3be7771dc70d154591c40124ccf8
Aareon Sullivan <askully13@gmail.com>
2018-03-20 11:47:31 -0700

    Add decimal precision

--- src/genesis.py
+++ src/genesis.py
@@ -1,4 +1,5 @@
 import base64
+from decimal import Decimal, getcontext
 import hashlib
 import logging
 import sys
@@ -10,7 +11,11 @@ import ecdsa
 from wallet import Wallet, load_wallet
 from ledger import Ledger, load_ledger
 
-logging.basicConfig(level=logging.INFO, filename="wallet.log", format='%(asctime)s %(message)s') # include timestamp
+# set up decimal precision
+getcontext().prec = 8
+
+# set up logger
+logging.basicConfig(level=logging.DEBUG, filename="wallet.log", format='%(asctime)s %(message)s') # include timestamp
 
 def create_genesis_transaction(recipient, private_key):
     timestamp = int(time.time() * 10000000)
@@ -19,7 +24,7 @@ def create_genesis_transaction(recipient, private_key):
     transaction = {'timestamp': timestamp,
                    'address': 'genesis',
                    'recipient': recipient,
-                   'amount': 100000000,
+                   'amount': 150000000,
                    'openfield': 'genesis'}
 
     
@@ -30,8 +35,11 @@ def create_genesis_transaction(recipient, private_key):
     # hash the transaction and generate a signature from the hash
     hashed_transaction = SHA256.new(str(transaction).encode('utf-8')).hexdigest()
     signature = sk.sign(hashed_transaction.encode('utf-8'))
+
+    # encode the signature to shorten the length
     encoded_signature = base64.b64encode(signature)
 
+    # hash a string-tuple containing the timestamp and the raw transaction to create the block hash
     block_hash = hashlib.sha256(str((timestamp, transaction)).encode('utf-8')).hexdigest()
 
     return transaction, encoded_signature, block_hash
@@ -43,19 +51,21 @@ if __name__ == '__main__':
 
     # check that no genesis block has already been generated
     last_block_height = ledger.query(Ledger.height).order_by(-Ledger.id).first()
+        
     if last_block_height is not None:
-        logging.info('Couldn\'t create genesis block, chain already exists!')
+        logging.debug('Couldn\'t create genesis block, chain already exists!')
         sys.exit(1)
+    else:
+        logging.debug('No previous blocks found, creating genesis block.')
 
     # get the most recently geneated address and private key from the wallet
     address, public_key, private_key = wallet.query(Wallet.address, Wallet.public_key, Wallet.private_key).first()
 
-    print('Sending genesis reward to address:', address)
-
     transaction, signature, block_hash = create_genesis_transaction(address, private_key)
 
+    print('Sending genesis reward of {} CHKN to address: {}'.format(Decimal(transaction['amount'])/100000000, transaction['recipient']))
+
     # store the genesis block in the ledger!
-    print(transaction['recipient'])
     ledger.add(Ledger(height=1,
                       timestamp=transaction['timestamp'],
                       address=transaction['address'],
