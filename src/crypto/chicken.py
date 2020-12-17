fcea046651149fd7cd87f63d8433e1f63041fb21
Aareon Sullivan <askully13@gmail.com>
2020-12-17 14:24:24 -0600

    Update chicken.py

--- src/crypto/chicken.py
+++ src/crypto/chicken.py
@@ -1,30 +1,5 @@
-from Cryptodome.Hash import SHA256, BLAKE2b, keccak
-from Cryptodome.Util.py3compat import bord
-
-import groestlcoin_hash
-
-
-class Groestl:
-    """ An almost compliant hashlib-like wrapper for the
-    Groestl hashing algorithm"""
-    __slots__ = ("data", "digest_bits", "hash",)
-
-    def __init__(self):
-        self.data = None
-        self.digest_bits = None
-        self.hash = None
-
-    def new(self, data):
-        self.data = data if isinstance(data, bytes) else data.encode("utf-8")
-        self.digest_bits = len(data)
-        self.hash = groestlcoin_hash.getHash(self.data, self.digest_bits)
-        return self
-    
-    def digest(self):
-        return self.hash
-    
-    def hexdigest(self):
-        return "".join(["%02x" % bord(x) for x in self.digest()])
+from hashlib import blake2b as BLAKE2b
+from hashlib import sha3_256 as keccak
         
 
 def chicken_hash(data: bytes):
@@ -37,11 +12,10 @@ def chicken_hash(data: bytes):
     Returns:
         A :class:`Keccak_Hash` hash object
     """
-    a = BLAKE2b.new(data=data, digest_bits=256).digest()
-    b = keccak.new(data=a, digest_bits=256).digest()
-    c = Groestl().new(b).digest()
-    return keccak.new(data=c, digest_bits=256)
+    a = BLAKE2b(data).digest()
+    b = keccak(a).digest()
+    return keccak(b)
 
 
 if __name__ == "__main__":
-    print(chicken_hash('test'.encode('utf-8')).hexdigest())
+    print(chicken_hash(bytes("test", encoding="utf-8").hexdigest())
