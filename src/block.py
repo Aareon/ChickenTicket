70dbdb059fce92675f08851eba0904d1c13db5aa
Aareon Sullivan <askully13@gmail.com>
2022-04-16 19:54:11 -0700

    Update block.py

--- src/block.py
+++ src/block.py
@@ -1,62 +1,98 @@
 from dataclasses import dataclass
 from decimal import Decimal
-import time
 from typing import List
+
 from crypto.chicken import chicken_hash
-from merkle import MerkleTree
-from mempool import MempoolTx
+from utils.merkle import MerkleTree
+from utils.time_tools import get_timestamp
+
+try:
+    import ujson as json
+
+    USING_UJSON = True
+except ImportError:
+    import json
+
+    USING_UJSON = False
+
+
+class BlockException(Exception):
+    """Base class for block related exceptions"""
 
 
 @dataclass
 class BlockHeader:
+    version: int
     previous_proof: str
     merkle_root: str
     timestamp: int
     nonce: int
+
+    def to_dict(self):
+        return {
+            "ver": self.version,
+            "prev_proof": self.previous_proof,
+            "merkle": self.merkle_root,
+            "time": self.timestamp,
+            "nonce": self.nonce,
+        }
+
+    def json(self):
+        return json.dumps(self.to_dict(), sort_keys=True)
+
+    def __str__(self):
+        return self.json()
 
 
 class Block:
-    id: int
+    version = int
+    idx: int
     previous_proof: str
     merkle_root: str
     timestamp: int
-    nonce: int
+    header: BlockHeader
 
-    def __init__(self, id: int, last_block, nonce, previous_proof: str=None, merkle_root:str=None, timestamp=None, reward: Decimal = None, recipients: List[str] = []):
-        self.id = id
+    def __init__(
+        self,
+        idx: int,
+        last_block,
+        nonce: int,
+        previous_proof: str = None,
+        merkle_root: str = None,
+        timestamp: int = None,
+        reward: Decimal = None,
+        transactions: List[str] = [],
+        version: int = 0x0,
+    ):
+        self.version = version
+        self.idx = idx
         self.last_block = last_block
         self.nonce = nonce
-        self.timestamp = timestamp or int(time.time())
-        self.tree = MerkleTree(hash_function=chicken_hash)
+        self.timestamp = timestamp or get_timestamp()
+        self.tree = MerkleTree()  # sha256 hashed merkle tree
         self.proof = None
         self.previous_proof = previous_proof
         self.merkle_root = merkle_root
         self.difficulty = self.calculate_difficulty()
+        self.reward = None
 
         if hasattr(last_block, "proof"):
             self.previous_proof = last_block.proof
 
-        self.transactions = []
+        self.transactions = transactions
 
-    @property
-    def is_missing(self) -> List:
-        missing = []
-        if self.merkle_root is None:
-            missing.append("merkle_root")
-        if self.proof is None:
-            missing.append("proof")
-        if self.previous_proof is None:
-            missing.append("previous_proof")
+    def to_dict(self):
+        return {
+            "idx": self.idx,
+            "header": self.header.to_dict(),
+            "last_block": self.last_block,
+            "reward": self.reward,
+            "txs": [tx.json() for tx in self.transactions],
+            "hash": self.proof,
+        }
 
-        if len(missing) > 0:
-            return missing
-
-    @property
-    def is_ready(self) -> bool:
-        if self.is_missing is not None and len(self.is_missing) > 0:
-            return False
-        else:
-            return True
+    def json(self):
+        return json.dumps(self.to_dict(), sort_keys=True)
 
     @property
     def header(self):
@@ -64,14 +100,18 @@ class Block:
             self.tree.make_tree()
 
         self.merkle_root = self.tree.get_merkle_root()
-        return BlockHeader(self.previous_proof, self.merkle_root, self.timestamp, self.nonce)
+        return BlockHeader(
+            self.version,
+            self.previous_proof,
+            self.merkle_root,
+            self.timestamp,
+            self.nonce,
+        )
 
     def add_transaction(self, tx):
-        if isinstance(tx, MempoolTx):
-            tx = tx.tx
         if tx not in self.transactions:
             self.transactions.append(tx)
-            self.tree.add_leaf(str(tx), True)
+            self.tree.add_leaf(tx.json(), True)
 
     def calculate_difficulty(self):
         if isinstance(self.last_block, Block):
@@ -95,37 +135,40 @@ class Block:
         # in this case, its every 150000th block
         period_count = (self.last_block.index + 1) // 101000
         period_count -= 2  # free periods, how many times the bomb can be ignored
-        bomb = 2 ** period_count
+        bomb = 2**period_count
 
         # calculation for target
         self.difficulty = (self.last_block.difficulty + offset * sign) + bomb
         return self.difficulty
 
     def hash(self):
-        self.proof = chicken_hash(str(self)).hexdigest()
+        data = self.to_dict()
+        # header contains the miner rewardee's address and
+        # can't be included in the process that validates the block
+        del data["header"]
+        del data["hash"]
+        self.proof = chicken_hash(json.dumps(data).encode()).hex()
         return self.proof
 
-    def __str__(self):
-        s = f"<Block(id: {self.id}, timestamp: {self.timestamp}, header: <{self.header}>, transactions: {self.transactions}"
-        if hasattr(self, "difficulty"):
-            s += f", difficulty: {self.difficulty}"
-        if hasattr(self, "proof"):
-            if self.proof is not None:
-                s += f", proof: {self.proof}"
-        s += ")>"
-        return s
+    def validate(self):
+        # TODO
+        # check height
+        # check proof
+        # check transactions
+        raise NotImplementedError
 
-    def __repr__(self):
-        return f"{str(self)[:-2]}, is_ready: {self.is_ready})>"
+    def __str__(self):
+        return self.json()
 
 
 if __name__ == "__main__":
     # test creating a block
-    block = Block(0, None, 69420)
+    block = Block(0, None, 69, reward=69)
     genesis = "genesis"
-    block.previous_proof = chicken_hash(genesis + "previous_proof").hexdigest()
+    block.previous_proof = chicken_hash((genesis + "previous_proof").encode()).hex()
 
-    from transaction import Transaction, TXVersion, Input, Output
+    from transaction import Input, Output, Transaction, TXVersion
+
     tx = Transaction()
     tx.id = 0
     tx.ver = TXVersion.ver1
@@ -133,7 +176,7 @@ if __name__ == "__main__":
 
     # genesis input and output
     genesis = "genesis"
-    ipt = Input(chicken_hash(genesis + "input").hexdigest(), 0)
+    ipt = Input(chicken_hash((genesis + "input").encode()).hex(), 0)
     tx.add_input(ipt)
 
     opt = Output("0x0", Decimal("1.0"))
@@ -141,6 +184,7 @@ if __name__ == "__main__":
 
     # create a key just to sign the tx
     from keys import KeyPair
+
     keys = KeyPair.new()
     tx.sign(keys)
 
