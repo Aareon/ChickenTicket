28f53d371f38bbe33be8fe3072d3d937197bc564
Aareon Sullivan <askully13@gmail.com>
2022-04-30 22:38:24 -0700

    Add recovery phrase dialog
    TODO add phrase confirmation dialog w randomly ordered buttons

--- src/simplegui.py
+++ src/simplegui.py
@@ -4,6 +4,7 @@ import threading
 import time
 from decimal import Decimal
 from pathlib import Path
+from random import randint
 
 import pyperclip as clip
 import PySimpleGUI as sg
@@ -33,35 +34,72 @@ if not wallet_fp.exists():
     wallet_fp = Path(sg.popup_get_folder("Select wallet folder"))
 
     if not (wallet_fp / "wallet.der").exists():
+
+        def phrase_layout():
+            # fmt: off
+            return [
+                [sg.Text("Recovery Phrase", font=("Arial 12 bold"))],
+                [sg.Text("Write the following words down somewhere safe!\nThis will be used to recover your wallet if you ever lock yourself out or lose the file.")],
+                [sg.Multiline(no_scrollbar=True, size=(30, 3), disabled=True, key='-words-')],
+                [sg.Button("Refresh", key="-refresh-"), sg.HSeparator(), sg.Button("Next", key="-next-")]
+            ]
+            # fmt: on
+
+        # generate a new wallet
+        # get 12 random words from mnemonics.txt
+        def generate_wordlist():
+            with open(Path(__file__).parent / "mnemonics.txt", "r") as f:
+                words = f.read().splitlines()
+                word_list = []
+                for _ in range(0, 12):
+                    r = randint(0, len(words))
+                    while words[r] in word_list:
+                        r = randint(0, len(words))
+                    else:
+                        word_list.append(words[r])
+                return word_list
+        
+        # show keyphrase
+        word_win = sg.Window("ChickenTicket Create Wallet", phrase_layout()).finalize()
+        word_win["-words-"].update(" ".join(w for w in generate_wordlist()))
+        while True:
+            event, values = word_win.read()
+            if event == sg.WIN_CLOSED:
+                if sg.popup("Are you sure you want to exit?", button_type=1) == "Yes":
+                    sys.exit(1)
+                else:
+                    word_win = sg.Window("ChickenTicket Create Wallet", phrase_layout()).finalize()
+                    word_win["-words-"].update(" ".join(w for w in generate_wordlist()))
+            elif event == "-refresh-":
+                word_win["-words-"].update(" ".join(w for w in generate_wordlist()))
+        word_win.close()
+
         kp = KeyPair.new()
         WALLET = Wallet()
         WALLET.create_wallet_address(kp)
         WALLET.save_to_der(wallet_fp / "wallet.der")
     else:
-        WALLET = Wallet.load_from_der(wallet_fp / "wallet.der")
+        WALLET = Wallet().load_from_der(wallet_fp / "wallet.der")
 
 else:
     print("Wallet exists. Loading...")
     WALLET = Wallet().load_from_der(wallet_fp)
 
-AVAILABLE = 0
-
-# fmt: off
-layout = [
-    # main layout
-    [sg.Text("Wallet", font="Arial 14 bold"), sg.Text("(Out of sync)", text_color="#f00", key='-sync-')],
-    [sg.Text("Balances", font="Arial 12 bold")],
-    [sg.Text("Available:"), sg.Text("0 CHKN", font=("Arial 10 bold"), key='-available-')],
-    [sg.Text("Pending:"), sg.Text("0 CHKN", font=("Arial 10 bold"), key='-pending-')],
-    [sg.HSeparator()],
-    [sg.Text("Total:"), sg.Text("0 CHKN", font=("Arial 10 bold"), key='-total-')],
-    [sg.Button("Send", key="-send-"), sg.VSeperator(), sg.Button("Receive", key="-receive-"), sg.VSeperator(), sg.Button("Settings", key='-settings-')],
-    [sg.ProgressBar(100, orientation='h', size=(20, 20), key='-sync progress-'), sg.Text("0 connections", key='-connections-')]
-]
-# fmt: on
-
 
 def run():
+    # fmt: off
+    layout = [
+        # main layout
+        [sg.Text("Wallet", font="Arial 14 bold"), sg.Text("(Out of sync)", text_color="#f00", key='-sync-')],
+        [sg.Text("Balances", font="Arial 12 bold")],
+        [sg.Text("Available:"), sg.Text("0 CHKN", font=("Arial 10 bold"), key='-available-')],
+        [sg.Text("Pending:"), sg.Text("0 CHKN", font=("Arial 10 bold"), key='-pending-')],
+        [sg.HSeparator()],
+        [sg.Text("Total:"), sg.Text("0 CHKN", font=("Arial 10 bold"), key='-total-')],
+        [sg.Button("Send", key="-send-"), sg.VSeperator(), sg.Button("Receive", key="-receive-"), sg.VSeperator(), sg.Button("Settings", key='-settings-')],
+        [sg.ProgressBar(100, orientation='h', size=(20, 20), key='-sync progress-'), sg.Text("0 connections", key='-connections-')]
+    ]
+    # fmt: on
     window = sg.Window("ChickenTicket simple GUI", layout)
     while True:
         event, values = window.read()
