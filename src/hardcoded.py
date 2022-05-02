90d021bc20b6eb414f90d6287b19635f680334a7
Aareon Sullivan <askully13@gmail.com>
2022-05-02 01:55:32 -0700

    Update hardcoded.py

--- src/hardcoded.py
+++ src/hardcoded.py
@@ -8,7 +8,7 @@ def generate_genesis_tx(genesis_wallet):
 
     i = Input("n0hash", 0)  # the block hash from which funds are located, and txid
     o = Output(
-        "0x00e9823286bee337a5f3965e3b2uau", 1000000000
+        genesis_wallet.addresses[0][0], 1000000000
     )  # address to send funds and amount
 
     tx = Transaction(
@@ -17,29 +17,21 @@ def generate_genesis_tx(genesis_wallet):
         inputs=[i],
         outputs=[o],
         pubkey="5109f8adff2e06cb4612d9fb337cb8981937f77328f9386ef7f5573d2cc4051ae8f268b7ecd2307f2fccdfa9d55cfc87d0d9aef2a8637effec9fa59da0c76e32",
-        timestamp=16506941462005280
+        timestamp=16506941462005280,
     )
     tx.hash()
-    assert (
-        tx.proof == "595ec60c2329d9a3ed5eb96188fe9a40efeac880afed98a2aa6dc1a1a062338e"
-    )
     tx.signature = "f6fa55c140f939c43a84ff871310963dc9b1257fb3aadf1a35e63b360dc16c4e07bd0ca8c8187816e3654f3f43fa530c1c2d55f854e379939d0f6f1b9045912a"
-    assert (
-        tx.json()
-        == '{"fee": "None", "hash": "595ec60c2329d9a3ed5eb96188fe9a40efeac880afed98a2aa6dc1a1a062338e", "idx": 0, "in": [{"idx": 0, "tx": "n0hash"}], "out": [{"amount": "1000000000", "recipient": "0x00e9823286bee337a5f3965e3b2uau"}], "pub": "5109f8adff2e06cb4612d9fb337cb8981937f77328f9386ef7f5573d2cc4051ae8f268b7ecd2307f2fccdfa9d55cfc87d0d9aef2a8637effec9fa59da0c76e32", "sig": "f6fa55c140f939c43a84ff871310963dc9b1257fb3aadf1a35e63b360dc16c4e07bd0ca8c8187816e3654f3f43fa530c1c2d55f854e379939d0f6f1b9045912a", "time": 16506941462005280, "ver": 1}'
-    )
     print(tx)
     return tx
 
 
 def generate_genesis_block(txs):
-    block = Block(
-        idx=0,
-        ver=1,
-        timestamp=get_timestamp()
-    )
-    for tx in txs:
-        block.add_transaction(tx)
+    block = Block(idx=0, ver=1, timestamp=get_timestamp())
+    if isinstance(txs, Transaction):
+        block.add_transaction(txs)
+    else:
+        for tx in txs:
+            block.add_transaction(tx)
     print(block)
     return block
 
