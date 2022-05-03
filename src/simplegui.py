b0f27c84f77d78a769dc5232256df719f49399d5
Aareon Sullivan <askully13@gmail.com>
2022-05-02 21:31:16 -0700

    Add theme support in settings window

--- src/simplegui.py
+++ src/simplegui.py
@@ -22,9 +22,90 @@ PENDING = 0
 SRC_DIR = Path(__file__).parent
 IMAGES_DIR = SRC_DIR.parent / "images"
 PEERS_LIST = SRC_DIR.parent / "peerslist.txt"
+SETTINGS_FP = SRC_DIR.parent / "sg_settings"
 print(f"Images dir: {IMAGES_DIR}")
 
-# attempt to load wallet file from default location
+sg.user_settings_filename(path=SETTINGS_FP)
+theme = sg.user_settings_get_entry("-theme-", "DarkBlue2")
+sg.user_settings_set_entry("-theme-", theme)
+
+
+def make_main_window():
+    sg.theme(sg.user_settings_get_entry("-theme-"))
+    # fmt: off
+    layout = [  # main layout
+        [sg.Text("Wallet", font="Arial 14 bold"), sg.Text("(Out of sync)", text_color="#f00", key='-sync-')],
+        [sg.Text("Balances", font="Arial 12 bold"), sg.Push(), sg.Text("Recent Transactions", font=("Arial 12 bold"))],
+        [sg.Text("Available:"), sg.Text("0 CHKN", font=("Arial 10 bold"), key='-available-')],
+        [sg.Text("Pending:"), sg.Text("0 CHKN", font=("Arial 10 bold"), key='-pending-')],
+        [sg.HSeparator()],
+        [sg.Text("Total:"), sg.Text("0 CHKN", font=("Arial 10 bold"), key='-total-')],
+        [sg.Button("Send", key="-send-"), sg.VSeperator(), sg.Button("Receive", key="-receive-"), sg.VSeperator(), sg.Button("Settings", key='-settings-')],
+        [sg.Text("Height: 0", font="Arial 9"), sg.ProgressBar(100, orientation='h', size=(20, 20), key='-sync progress-'), sg.Text("0 connections", key='-connections-')]
+    ]
+    # fmt: on
+    return sg.Window("ChickenTicket simple GUI", layout)
+
+
+def make_send_window():
+    # sg.theme(sg.user_settings_get_entry('-theme-', 'DarkBlue2'))
+    return sg.Window(
+        "Send",
+        [  # send window layout
+            [sg.Text("Send", font="Arial 12 bold")],
+            [
+                sg.Input(key="-input-"),
+                sg.Image(str(IMAGES_DIR / "red.png"), size=(20, 20), key="-status-"),
+            ],
+            [sg.Text("Fee: 0.0 CHKN", key="-fee-")],  # estimated transaction fee
+            [sg.HSeparator()],
+            [sg.Text("Available:"), sg.Text(f"{AVAILABLE} CHKN", font="Arial 10 bold")],
+            [sg.Text("Pending:"), sg.Text(f"{PENDING} CHKN", font="Arial 10 bold")],
+            [
+                sg.Button("Cancel", key="-cancel-"),
+                sg.HSeparator(),
+                sg.Button("Check", key="-check-"),
+                sg.Button("Send", key="-send-"),
+            ],
+        ],
+    )
+
+
+def make_receive_window(address):
+    # sg.theme(sg.user_settings_get_entry('-theme-', 'DarkBlue2'))
+    sg.Window(
+        "Receive",
+        [  # receive window layout
+            [sg.Image(str(IMAGES_DIR / "addressqr.png"))],
+            [sg.Text(f"Address: {address}"), sg.Button("Copy", key="-copy-")],
+            [sg.Button("OK", key="-ok-")],
+        ],
+    )
+
+
+def make_settings_window():
+    # sg.theme(sg.user_settings_get_entry('-theme-', 'DarkBlue2'))
+    theme_name_list = sg.theme_list()
+    return sg.Window(
+        "Settings",
+        [  # settings window layout
+            [sg.Text("Look and Feel", font="Arial 12 bold")],
+            [
+                sg.Text("UI Theme", font="Arial 10"),
+                sg.Listbox(
+                    theme_name_list,
+                    default_values=[sg.user_settings_get_entry("-theme-")],
+                    size=(15, 10),
+                    key="-theme_choice-",
+                ),
+            ],
+            [
+                sg.Push(),
+                sg.Button("Apply", key="-apply-"),
+                sg.Button("Done", key="-done-"),
+            ],
+        ],
+    )
 
 
 def run():
@@ -160,21 +241,8 @@ def run():
     )  # flask thread
     node_thread.start()
 
-    # fmt: off
-    layout = [  # main layout
-        [sg.Text("Wallet", font="Arial 14 bold"), sg.Text("(Out of sync)", text_color="#f00", key='-sync-')],
-        [sg.Text("Balances", font="Arial 12 bold"), sg.Push(), sg.Text("Recent Transactions", font=("Arial 12 bold"))],
-        [sg.Text("Available:"), sg.Text("0 CHKN", font=("Arial 10 bold"), key='-available-')],
-        [sg.Text("Pending:"), sg.Text("0 CHKN", font=("Arial 10 bold"), key='-pending-')],
-        [sg.HSeparator()],
-        [sg.Text("Total:"), sg.Text("0 CHKN", font=("Arial 10 bold"), key='-total-')],
-        [sg.Button("Send", key="-send-"), sg.VSeperator(), sg.Button("Receive", key="-receive-"), sg.VSeperator(), sg.Button("Settings", key='-settings-')],
-        [sg.Text("Height: 0", font="Arial 9"), sg.ProgressBar(100, orientation='h', size=(20, 20), key='-sync progress-'), sg.Text("0 connections", key='-connections-')]
-    ]
-    # fmt: on
-
     # Main wallet window
-    window = sg.Window("ChickenTicket simple GUI", layout)
+    main_window = make_main_window()
 
     def connections_changed(conns: int):
         """Callback to use when node connections changes
@@ -185,22 +253,12 @@ def run():
 
     node.connect_cb = connections_changed
     while True:
-        event, values = window.read()
+        event, values = main_window.read()
         if event == sg.WIN_CLOSED:
             break
         if event == "-send-":  # Send popup window
 
-            # fmt: off
-            send_win = sg.Window("Send", [  # send window layout
-                [sg.Text("Send", font="Arial 12 bold")],
-                [sg.Input(key="-input-"), sg.Image(str(IMAGES_DIR / "red.png"), size=(20, 20), key='-status-')],
-                [sg.Text("Fee: 0.0 CHKN", key="-fee-")],  # estimated transaction fee
-                [sg.HSeparator()],
-                [sg.Text("Available:"), sg.Text(f"{AVAILABLE} CHKN", font="Arial 10 bold")],
-                [sg.Text("Pending:"), sg.Text(f"{PENDING} CHKN", font="Arial 10 bold")],
-                [sg.Button("Cancel", key="-cancel-"), sg.HSeparator(), sg.Button("Check", key="-check-"), sg.Button("Send", key="-send-")]
-            ])
-            # fmt: on
+            send_win = make_send_window()
 
             while True:  # send window loop
                 event, vals = send_win.read()
@@ -247,20 +305,14 @@ def run():
 
         if event == "-receive-":  # Receive popup window
             # Receive button pressed, show address popup
-            window[event].update(disabled=True)
+            main_window[event].update(disabled=True)
             address = WALLET.addresses[0][0]
             qr = qrcode.make(address, box_size=6)
             if not IMAGES_DIR.exists():
                 images_dir.mkdir(parents=True)
             qr.save(IMAGES_DIR / "addressqr.png")
-            # fmt: off
 
-            rx_win = sg.Window("Receive", [  # receive window layout
-                [sg.Image(str(IMAGES_DIR / "addressqr.png"))],
-                [sg.Text(f"Address: {address}"), sg.Button("Copy", key="-copy-")],
-                [sg.Button("OK", key='-ok-')]
-            ])
-            # fmt: on
+            rx_win = make_receive_window()
 
             while True:  # receive window loop
                 event, _ = rx_win.read()
@@ -271,8 +323,33 @@ def run():
                     clip.copy(str(address))
             rx_win.close()
 
+        if event == "-settings-":  # settings popup window
+            settings_win = make_settings_window()
+
+            while True:  # settings window loop
+                event, values = settings_win.read()
+                if event == sg.WIN_CLOSED:
+                    break
+                elif event == "-apply-":
+                    if values["-theme_choice-"][0] != sg.user_settings_get_entry(
+                        "-theme-"
+                    ):
+                        sg.user_settings_set_entry(
+                            "-theme-", values["-theme_choice-"][0]
+                        )  # set theme to choice
+                        sg.theme(sg.user_settings_get_entry("-theme-"))
+                        # reset windows
+                        main_window.close()
+                        settings_win.close()
+                        main_window = make_main_window()
+                        settings_win = make_settings_window()
+                elif event == "-done-":
+                    break
+
+            settings_win.close()
+
     print("Closing!")
-    window.close()
+    main_window.close()
     sys.exit(0)
 
 
