43318f204e1a61c1fd74db2dc32e7fe3be3a890b
Aareon <askully13@gmail.com>
2023-04-18 16:54:56 -0500

    add confirmation window

--- src/ui/simplegui/recovery_phrase_window.py
+++ src/ui/simplegui/recovery_phrase_window.py
@@ -32,12 +32,37 @@ class RecoveryPhraseWindow:
 
     def setup(self) -> None:
         # fmt: off
-        word_list = generate_recovery_phrase()
         self.layout = [
             [sg.Text("Recovery Phrase", font=("Arial 12 bold"))],
             [sg.Text("Write the following words down somewhere safe!\nThis will be used to recover your wallet if you ever lock yourself out or lose the file.")],
-            [sg.Multiline(" ".join(w for w in word_list), no_scrollbar=True, size=(30, 3), disabled=True, key='-words-')],
+            [sg.Multiline(" ".join(w for w in self.parent.word_list), no_scrollbar=True, size=(30, 3), disabled=True, key='-words-')],
             [sg.Button("Refresh", key="-refresh-"), sg.HSeparator(), sg.Button("Next", key="-next-")]
+        ]
+        # fmt: on
+
+
+class ConfirmRecoveryPhraseWindow:
+    def __init__(self, parent=None) -> None:
+        self.parent = parent
+        self.title = "ChickenTicket - Confirm Recovery Phrase"
+        self.setup()
+        self.window = sg.Window(self.title, self.layout)
+    
+    def setup(self) -> None:
+        rows = [[], [], []]
+        x = 0
+        for i in range(3):
+            for j in range(4):
+                n = randint(0, len(self.parent.word_list) - 1)
+                rows[i].append(sg.Button(self.parent.word_list.pop(n), key=f"-word{x}-"))
+                x += 1
+        # fmt: off
+        self.layout = [
+            [sg.Text("Confirm your recovery phrase", font=("Arial 10 bold"))],
+            [sg.Multiline(no_scrollbar=True, disabled=True, size=(30, 3), key="-words-")],
+            rows,
+            [sg.HSeparator()],
+            [sg.Button("Reset", key="-reset-"), sg.HSeparator(), sg.Button("Confirm", disabled=True, key="-confirm-")]
         ]
         # fmt: on
 
@@ -46,10 +71,17 @@ if __name__ == "__main__":
     import sys
 
     class FakeApp:
-        pass
+        word_list = generate_recovery_phrase()
 
     app = FakeApp()
     win = RecoveryPhraseWindow(app)
+    while True:
+        event, values = win.window.read()
+        if event in (sg.WIN_CLOSED, "-cancel-"):
+            break
+    win.window.close()
+
+    win = ConfirmRecoveryPhraseWindow(app)
     while True:
         event, values = win.window.read()
         if event in (sg.WIN_CLOSED, "-cancel-"):
